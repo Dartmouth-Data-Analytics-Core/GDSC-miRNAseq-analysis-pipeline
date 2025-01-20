16c3456b3a24490180cd432a28d3a25a81306e49
shannonmargaret <shannon.soucy@uconn.edu>
2025-01-20 14:12:44 -0500

    Add files via upload
    Add a submission script

---
+++ job.script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Name of the job
+#SBATCH --job-name=mirna
+
+# Number of compute nodes
+#SBATCH --nodes=1
+
+# partition
+#SBATCH --partition=preempt1
+
+# account
+#SBATCH --account=dac
+
+# Walltime (job duration)
+#SBATCH --time=60:00:00
+
+# Email notifications (comma-separated options: BEGIN,END,FAIL)
+#SBATCH --mail-type=FAIL
+
+source /optnfs/common/miniconda3/etc/profile.d/conda.sh
+
+conda activate /dartfs/rc/nosnapshots/G/GMBSR_refs/envs/snakemake
+
+snakemake -s Snakefile --profile cluster_profile -T 2 --use-conda --conda-frontend conda --conda-prefix /dartfs/rc/nosnapshots/G/GMBSR_refs/envs/miRNAseq
